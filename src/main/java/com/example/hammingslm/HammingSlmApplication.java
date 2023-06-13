85a81e6b4bfae1539274b239cab973bd0cbd25c7
PaulOgris <131256574+PaulOgris@users.noreply.github.com>
2023-06-13 11:41:06 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingslm/HammingSlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingSlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammingSlmApplication.class, args);
+	}
+
+}
